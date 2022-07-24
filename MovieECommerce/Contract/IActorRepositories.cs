5190ea2e2d75496c055186062788ba5e12f15639
IfunanyaWilliam <williamifunanya@gmail.com>
2022-07-24 16:36:07 +0100

    Implement GetActor(), AddActor(), UpdateActor(), DeleteActor() methods in ActorRepository

--- MovieECommerce/Contract/IActorRepositories.cs
+++ MovieECommerce/Contract/IActorRepositories.cs
@@ -1,9 +1,18 @@
 ﻿using MovieECommerce.Models;
+using System.Linq.Expressions;
 
 namespace MovieECommerce.Contract
 {
     public interface IActorRepository
     {
         Task<IEnumerable<Actor>> GetActorsAsync();
+
+        Task<Actor> GetActor(Expression<Func<Actor, bool>> predicate);
+
+        Task<bool> AddActor(Actor actor);
+
+        Task<bool> UpdateActor(Actor actor);
+
+        Task<bool> DeleteActor(string actorId);
     }
 }
--- MovieECommerce/Repositories/ActorRepository.cs
+++ MovieECommerce/Repositories/ActorRepository.cs
@@ -2,6 +2,7 @@
 using MovieECommerce.Contract;
 using MovieECommerce.Data;
 using MovieECommerce.Models;
+using System.Linq.Expressions;
 
 namespace MovieECommerce.Repositories
 {
@@ -19,6 +20,37 @@ namespace MovieECommerce.Repositories
             return await _context.Actors.ToListAsync();
         }
 
+        public async Task<Actor> GetActor(Expression<Func<Actor, bool>> predicate)
+        {
+            return await _context.Actors.AsQueryable().FirstOrDefaultAsync(predicate);
+        }
 
+        public async Task<bool> AddActor(Actor actor)
+        {
+            await _context.Actors.AddAsync(actor);
+            return await _context.SaveChangesAsync() > 0;
+        }
+        
+        public async Task<bool> UpdateActor(Actor actor)
+        {
+            var newActor = _context.Actors.Find(actor.ActorId);
+            if(newActor is not null)
+            {
+                _context.Actors.Update(newActor);
+                return await _context.SaveChangesAsync() > 0;
+            }
+            return false;
+        }
+
+        public async Task<bool> DeleteActor(string actorId)
+        {
+            var actor = await _context.Actors.FirstOrDefaultAsync(x => x.ActorId == actorId);
+            if(actor is not null)
+            {
+                _context.Actors.Remove(actor);
+                return await _context.SaveChangesAsync() > 0;
+            }
+            return false;
+        }
     }
 }
