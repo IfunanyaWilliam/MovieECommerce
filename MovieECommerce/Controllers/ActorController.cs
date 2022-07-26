5501d0639a5c027ca5d3e3c57da88a906d27d08a
IfunanyaWilliam <williamifunanya@gmail.com>
2022-07-26 16:11:58 +0100

    Add AutoMapper for mapping from Model to ViewModel

--- MovieECommerce/Controllers/ActorController.cs
+++ MovieECommerce/Controllers/ActorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using MovieECommerce.Contract;
 using MovieECommerce.Models;
 using MovieECommerce.ViewModels;
@@ -8,10 +9,16 @@ namespace MovieECommerce.Controllers
     public class ActorController : Controller
     {
         private readonly IActorRepository _actorRepo;
+        private readonly IImageService _imageService;
+        private readonly IMapper _mapper;
 
-        public ActorController(IActorRepository actorRepo)
+        public ActorController(IActorRepository actorRepo, 
+                               IImageService imageService,
+                               IMapper mapper)
         {
             _actorRepo = actorRepo;
+            _imageService = imageService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
@@ -29,11 +36,35 @@ namespace MovieECommerce.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ActorViewModel model)
         {
-            var actor = new Actor();
-            if (ModelState.IsValid)
+            var actor = _mapper.Map<Actor>(model); ;
+            if (!ModelState.IsValid)
             {
-                var result = await _actorRepo.AddActor(actor);
+               return View(model);
             }
+
+            //Add Actor photo to Cloudinary and generate absolute path
+            string imgUrl = "";
+
+            if (model.ProfilePictureURL is not null)
+            {
+                var filePath = Path.GetTempFileName();
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await model.ProfilePictureURL.CopyToAsync(stream);
+                }
+                var uploadResult = await _imageService.UploadImage(filePath);
+                imgUrl = uploadResult;
+            }
+
+            actor.ProfilePictureURL = imgUrl;
+            var result = await _actorRepo.AddActor(actor);
+            if (result)
+            {
+                TempData["ActorAddedSuccessfully"] = "Actor successfully added.";
+                return RedirectToAction("Index");
+            }
+            TempData["Error"] = "Actor could not be added";
             return View(model);
         }
     }
---
+++ MovieECommerce/MappingProfiles/ActorProfile.cs
@@ -0,0 +1,14 @@
+﻿using MovieECommerce.Models;
+using MovieECommerce.ViewModels;
+
+namespace MovieECommerce.MappingProfiles
+{
+    public class ActorProfile : AutoMapper.Profile
+    {
+        public ActorProfile()
+        {
+            CreateMap<ActorViewModel, Actor>();
+            CreateMap<Actor, ActorViewModel>();
+        }
+    }
+}
